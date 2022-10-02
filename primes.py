6dd9efb5ab1b230706ea6fdcea6c28e16de4d78a
Daniel Holland <daniel.1.holland@kcl.ac.uk>
2022-10-02 11:42:30 +0100

    Adding rasied exception statement

--- primes.py
+++ primes.py
@@ -2,5 +2,26 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <= 0:
+        raise ValueError
+    else:
+        list = [2]
+        counter = 3
+        baseCounter = 1
+        isPrime = False
+        if number_of_primes == 1:
+            return list
+        else:
+            while baseCounter != number_of_primes:
+                isPrime = False
+                for x in range(2, counter):
+                    if (counter % x) == 0:
+                        isPrime = False
+                        break
+                    else:
+                        isPrime = True
+                if isPrime:
+                    list.append(counter)
+                    baseCounter += 1
+                counter += 1
+            return list
